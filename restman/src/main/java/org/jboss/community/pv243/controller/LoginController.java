22824090a8ada8de735eb9d9d52c2a73acdcbb0b
rhopp <rhopp@redhat.com>
2012-06-20 10:20:14 +0200

    First version of programmatic login

--- restman/src/main/java/org/jboss/community/pv243/controller/LoginController.java
+++ restman/src/main/java/org/jboss/community/pv243/controller/LoginController.java
@@ -1,9 +1,13 @@
 package org.jboss.community.pv243.controller;
 
+import javax.annotation.security.RolesAllowed;
 import javax.enterprise.inject.Model;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
-import javax.inject.Named;
+import javax.security.auth.login.LoginContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @Model
@@ -13,8 +17,52 @@ public class LoginController {
 	FacesContext facesContext;
 	
 	public void logout(){
-		System.out.println("Odhlasuju");
+		//facesContext.getExternalContext().getSessionMap().put("sourcePageForLogin", facesContext.getExternalContext().getRequestPathInfo())
 		facesContext.getExternalContext().invalidateSession();
+		HttpServletRequest request = (HttpServletRequest)facesContext.getExternalContext().getRequest();
+		try {
+			request.logout();
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
 	}
 
+	public void login(){
+		HttpServletRequest request = (HttpServletRequest)facesContext.getExternalContext().getRequest();
+		HttpServletResponse response = (HttpServletResponse)FacesContext.getCurrentInstance().getExternalContext().getResponse();
+		try {
+			if (!request.authenticate(response)) {
+			    FacesContext.getCurrentInstance().responseComplete();
+			}
+		} catch (Exception e) { // may throw ServletException or IOException
+		    e.printStackTrace();
+		}
+		return;
+	}
+	
+	public boolean isLogged(){
+		return facesContext.getExternalContext().getUserPrincipal() != null;
+	}
+	
+	public boolean isManager(){
+		if (facesContext.getExternalContext().isUserInRole("MANAGER")){
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isUser(){
+		if (facesContext.getExternalContext().isUserInRole("USER")){
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isAdmin(){
+		if (facesContext.getExternalContext().isUserInRole("ADMIN")){
+			return true;
+		}
+		return false;
+	}
+	
 }
